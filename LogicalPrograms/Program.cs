8e95ecc96f394237d6d151f64d3904e645d8841a
pampapathi73 <pampapathik32@gmail.com>
2021-05-30 10:06:39 +0530

    [pampapathi] Added : Fibonacci Series

--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -7,6 +7,22 @@ namespace LogicalPrograms
         static void Main(string[] args)
         {
             Console.WriteLine("welcome to logical programs");
+            Console.WriteLine("Enter your choice ");
+            Console.WriteLine(".............................................");
+            Console.WriteLine("1 For: Fibonacci Series ");
+            int selection = Convert.ToInt32(Console.ReadLine());
+            switch (selection)
+            {
+                case 1:
+                    //Fibonacco Series
+                    fibonacciSeries FlibSeries = new fibonacciSeries();
+                    FlibSeries.CheckFibonacciSeries();
+                    break;
+                default:
+                    Console.WriteLine("Invalid Selection. Select in b/w  1 to 4");
+                    break;
+            }
+            Console.ReadLine();
         }
     }
 }
---
+++ LogicalPrograms/fibonacciSeries.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogicalPrograms
+{
+    class fibonacciSeries
+    {
+        public int Number1 = 0;
+        public int Number2 = 1;
+        public int Number3;
+        public int Var_i;
+        public int ReadNumber;
+        public void CheckFibonacciSeries()
+        {
+
+            Console.Write("Enter the number of elements: "); //Input From User
+            ReadNumber = int.Parse(Console.ReadLine()); //Read Number 
+            Console.WriteLine(Number1 + " " + Number2 + " "); //printing 0 and 1    
+            for (Var_i = 2; Var_i < ReadNumber; ++Var_i) //loop starts from 2 because 0 and 1 are already printed    
+            {
+                Number3 = Number1 + Number2; //adding Number1 + Number 2 and stroe it in Number 3
+                Console.WriteLine(Number3 + " ");  // print Number3
+                Number1 = Number2; //assign value
+                Number2 = Number3;//assign value
+            }
+
+        }
+    }
+}
