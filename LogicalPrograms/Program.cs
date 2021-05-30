0a59c1a9cbfd559e26e897a9320f608fa479b182
pampapathi73 <pampapathik32@gmail.com>
2021-05-30 11:01:45 +0530

    [pampapathi] Added : reverse given number

--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -12,6 +12,7 @@ namespace LogicalPrograms
             Console.WriteLine("1 For: Fibonacci Series ");
             Console.WriteLine("2 For: Perfect Number ");
             Console.WriteLine("3 For: Prime Number ");
+            Console.WriteLine("4 For: Reverse Number ");
             int selection = Convert.ToInt32(Console.ReadLine());
             switch (selection)
             {
@@ -29,6 +30,11 @@ namespace LogicalPrograms
                     //Prime Number
                     PrimeNumber Primenumber = new PrimeNumber();
                     Primenumber.CheckPrimeNumber();
+                    break;
+                case 4:
+                    //Reverese Number
+                    ReverseANumber reverseNum = new ReverseANumber();
+                    reverseNum.ReverseNumber();
                     break;
                 default:
                     Console.WriteLine("Invalid Selection. Select in b/w  1 to 4");
